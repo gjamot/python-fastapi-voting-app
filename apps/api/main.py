e9cd15ce262c3158e643083f6e09ffcf0a0ccedb
gjamot <gregory.jamot@gmail.com>
2025-09-02 15:09:14 +0200

    Adding new endpoints to api layer.

--- apps/api/main.py
+++ apps/api/main.py
@@ -21,6 +21,51 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/models/{model_name}")
+async def get_model(model_name: models.ModelName):
+    if model_name is models.ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 @app.get("/api/posts/{post_id}")
 def read_post(post_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
@@ -28,4 +73,23 @@ def read_post(post_id: int, db: Session = Depends(get_db)):
         return {"id": post.id, "title": post.title}
     else:
         raise HTTPException(status_code=404, detail="Post not found")
+    
+@app.post("/api/posts/")
+async def create_post(post_info: models.PostInfo, db: Session = Depends(get_db)):
+    post = models.Post(title=post_info.title)
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
+
+@app.put("/api/posts/{post_id}")
+async def update_post(post_id: int, post_info: models.PostInfo, db: Session = Depends(get_db)):
+    query = db.query(models.Post).filter_by(id=post_id)
+    post = query.first
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    query.update(post_info.model_dump(), synchronize_session=False)
+    db.commit()
+    db.refresh(post)
+    return post
 
--- apps/api/models.py
+++ apps/api/models.py
@@ -1,8 +1,18 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from enum import Enum
 
 Base = declarative_base()
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+class PostInfo(BaseModel):
+    title: str
+    
 class Post(Base):
     __tablename__ = "posts"
     __table_args__ = {"schema": "fastapi_voting_app"}
